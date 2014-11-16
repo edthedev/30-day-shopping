a3e9e35aab9b3060d48eb87af7c3fb6d5b428917
Edward Delaporte <edthedev@gmail.com>
2014-11-15 21:03:53 -0600

    Starting add form.

--- app.py
+++ app.py
@@ -2,6 +2,7 @@
 #-----------
 from peewee import Model, SqliteDatabase
 from peewee import CharField, DateField, BooleanField, DecimalField
+from peewee import OperationalError
 
 db = SqliteDatabase('shopping.db')
 
@@ -13,24 +14,42 @@ class Purchase(Model):
     bought = BooleanField()
 
     class Meta:
-        database = db 
+        database = db
 
 # Web forms
 # ------------
 from wtfpeewee.orm import model_form
 PurchaseForm = model_form(Purchase)
+PurchaseForm.csrf = True
 
 # Web app
 #----------
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
+    kwargs = {}
     form = PurchaseForm()
-    return render_template('template.html', form=form)
+
+    # Maybe add an item.
+    if request.method == 'POST':
+        purchase = Purchase()
+        form = PurchaseForm(request.form, obj=purchase)
+        if form.validate():
+            form.populate_obj(purchase)
+            purchase.save()
+
+    # Show add form and previously added items.
+    kwargs['form'] = form
+    kwargs['purchases'] = Purchase.select()
+    return render_template('template.html', **kwargs)
 
 if __name__ == '__main__':
+    try:
+        db.create_tables([Purchase])
+    except OperationalError:
+        print "Skipping table creation..."
     app.debug = True
     app.run()
