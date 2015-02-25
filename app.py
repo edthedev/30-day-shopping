46b19a85c0802282b3e2f7615380bfb7bcf4f908
Edward Delaporte <edthedev@gmail.com>
2015-02-24 20:32:19 -0600

    Allow put for modify.

--- app.py
+++ app.py
@@ -120,7 +120,7 @@ manager = flask.ext.restless.APIManager(app, flask_sqlalchemy_db=db)
 
 # Create API endpoints, which will be available at /api/<tablename> by
 # default. Allowed HTTP methods can be specified as well.
-manager.create_api(Purchase, methods=['GET', 'POST', 'DELETE'])
+manager.create_api(Purchase, methods=['GET', 'PUT', 'POST', 'DELETE'])
 
 
 
